a74f41a80bc8b3bd7718e4ca007287bb374444a6
Rabin <rabin.shrestha7@gmail.com>
2023-06-15 18:56:13 -0700

    curd operation'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { FotterComponent } from './fotter/fotter.component';
+import { FrameComponent } from './frame/frame.component';
+import { NavComponent } from './nav/nav.component';
+import { BodyIIComponent } from './body-ii/body-ii.component';
+import { RecordTblComponent } from './record-tbl/record-tbl.component';
+import { EditShwComponent } from './edit-shw/edit-shw.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"body",component:BodyComponent},
+  {path:"fotter",component:FotterComponent},
+  {path:"frame",component:FrameComponent},
+  {path:"nav",component:NavComponent},
+  {path:'bodyii',component:BodyIIComponent},
+  {path:'recordTbl',component:RecordTblComponent},
+  {path:'edit/:id',component:EditShwComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  constructor(){
+
+  }
+ngOnInit(): void {
+    
+} 
+
+}
